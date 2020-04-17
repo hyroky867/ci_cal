ff0b69182a8e8696660bb270c73c4dbc9b8bbea4
hyroky867 <handm755@gmail.com>
2020-04-17 22:57:13 +0900

    phpcs phpmd をインストール

---
+++ bin/setup-pre-commit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd `dirname $0`/..``
+APP_DIR=`pwd`
+
+cp -f $APP_DIR/bin/pre-commit $APP_DIR/.git/hooks/ && cp -f $APP_DIR/bin/pre-commit-config $APP_DIR/.git/hooks/ && chmod +x $APP_DIR/.git/hooks/pre-commit
